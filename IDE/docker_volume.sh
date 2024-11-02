c944fbbbb996b11fb00c4436766d7ee39f25f2a0
junwon-song <thdqls12@gmail.com>
2024-11-02 16:29:04 +0900

    docker push

--- IDE/docker_volume.sh
+++ IDE/docker_volume.sh
@@ -1,15 +1,9 @@
 # 볼륨 새성 (volume name : cloud-cicd-src)
-docker volume create  \
---opt device="/Users/dangtongbyun/Library/CloudStorage/GoogleDrive-dbyun@redhat.com/내 드라이브/05.Lecture/cicd/cloud-cicd/src" \
---opt o=bind --opt type=none cloud-cicd-src
+docker volume create  --opt device="C:\Users\CICD\cicd\cicd-start\src" --opt o=bind --opt type=none cloud-cicd-src
 
-docker volume create  \
---opt device="/Users/dangtongbyun/Library/CloudStorage/GoogleDrive-dbyun@redhat.com/내 드라이브/05.Lecture/cicd/cloud-cicd/vscode" \
---opt o=bind --opt type=none vscode
+docker volume create  --opt device="C:\Users\CICD\cicd\cicd-start\vscode" --opt o=bind --opt type=none vscode
 
-docker volume create  \
---opt device="/Users/dangtongbyun/Library/CloudStorage/GoogleDrive-dbyun@redhat.com/내 드라이브/05.Lecture/cicd/cloud-cicd/jenkins" \
---opt o=bind --opt type=none jenkins-home
+docker volume create  --opt device="C:\Users\CICD\cicd\cicd-start\jenkins" --opt o=bind --opt type=none jenkins-home
 
 # 볼륨 생성 확인
 docker volume ls
